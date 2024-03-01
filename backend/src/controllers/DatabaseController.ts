06b7a37863688edbcc6f4b9d17c2bab5efaa425b
Elias Eggenberger <elias.eggenberger@hm.edu>
2024-03-01 21:16:02 +0100

    Frontend Struktur aufgesetzt

--- backend/src/controllers/DatabaseController.ts
+++ backend/src/controllers/DatabaseController.ts
@@ -8,6 +8,7 @@ import {getAllData} from "../services/DatabaseService"
 export const GetAllEntries = async (request: Request, response: Response) => {
     console.log("ConstrollerHit")
     const dataResp = await getAllData()
+    
     response.send(dataResp) 
   
   };
---
+++ frontend/frontendui/app/page.tsx
@@ -0,0 +1,13 @@
+
+  export default function LandingPage() {
+    return(
+      <div>
+        <nav style={{height :"20px", backgroundColor: 'black', color: 'white' }}>
+          <h1>
+            Hallo Welt
+          </h1>
+        </nav>
+      </div>
+    )
+  } 
+
