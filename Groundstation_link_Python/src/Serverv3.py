2c69ec2fbd80c83acab64c1d26676c38457fd118
Fynn Gewiese <gewiese@hm.edu>
2024-03-02 15:54:21 +0100

    updates Serverv3

--- Groundstation_link_Python/src/Serverv3.py
+++ Groundstation_link_Python/src/Serverv3.py
@@ -2,7 +2,8 @@ import socket
 import struct
 import threading
 import time
-from MongoDB import MongoDB 
+import os
+# from MongoDB import MongoDB 
 
 # ip_uC = '192.168.178.23'
 ip_Laptop = '169.254.171.44'
@@ -30,8 +31,7 @@ def get_ip_address():
     
 class TCP_SERVER:
     def __init__(self, _struct_format="L L 6L 6f 6f 6i i f 2i 80s"):
-        self.packets = []
-        self.rawdata = []
+        self.datalog = DATALOGGER()
         self.ipadress= ip_Laptop
         self.port = 8888
         self.isRunning = 1
@@ -46,6 +46,7 @@ class TCP_SERVER:
         print(f'- starting TCP server at "{self.ipadress}" at "{time.asctime(time.localtime())}"')
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Erstellen Sie einen Socket für den Server
         
+        #tries to connect to sockel
         while True:
             try:
                 server_socket.bind((self.ipadress, self.port))  # Binden Sie den Socket an den Host und Port
@@ -63,8 +64,6 @@ class TCP_SERVER:
             except Exception as e: # Handle other exceptions  
                 print("Error:", e)
 
-        
-        
         def ConnectClient():
             """wartet auf einen Client und verbindet auch mit demselben"""
             print('- Server wartet auf Verbindung',end='')
@@ -74,7 +73,8 @@ class TCP_SERVER:
             client_socket.settimeout(2)
             print(' | Verbindung hergestellt von:', client_address)
             return client_socket, client_address
-            
+        
+        #first connection with Client
         client_socket, client_address = ConnectClient()
         timestamp_server = millis()-1 #-1 damit später be der packet/s berechnung kein 0 unterm bruch landen kann
             
@@ -84,12 +84,14 @@ class TCP_SERVER:
             try:
                 timestamp= time.asctime(time.localtime())[::-1][::-1]
                 received_data = client_socket.recv(200)
+                self.datalog.rawdata.append((timestamp,received_data))
                 if not received_data:
                     print("\n duno wtf this is")
                     print(f"Length of Recieved Data: {len(received_data)}")
                     print(f"data where error occured {received_data}")
                     client_socket, client_address = ConnectClient()
                     continue
+                    
                 
             except socket.timeout:
                 print("")
@@ -112,132 +114,53 @@ class TCP_SERVER:
             
             counter_recieved += 1
             
-            #refreshed output ab 1000 recieved packages nur noch alle 100 recieved packages
+            #refreshed terminal info ab 1000 recieved packages nur noch alle 100 recieved packages
             if counter_recieved<1000 or counter_recieved % 100 == 0: 
                 print("\r" + " " * 70 + "\r", end='', flush=True)  # Clear the  last line
-                print(f'- recieved_structs: {counter_recieved} | sizeof(latest_data): {len(received_data)} bytes | corrupted: {counter_corrupted} | packets/sec: {round(1000*counter_recieved/(millis()-timestamp_server),2)}', end='',flush=True)
+                print(f'- recieved_structs: {counter_recieved} | sizeof(latest_data): {len(received_data)} bytes | corrupted wip: {counter_corrupted} | packets/sec: {round(1000*counter_recieved/(millis()-timestamp_server),2)}', end='',flush=True)
 
 
-               #convert binary to struct
-            try:
-                self.rawdata.append((received_data,time.time()))
-                unpacked_data = struct.unpack(self.struct_format, received_data) # Entpacken Sie die Daten in die Struktur
-                self.packets.append((unpacked_data,timestamp))
-            except:
-                print("\nstruct_format is wrong or data is corrupted")
-                print(f"Length of Recieved Data: {len(received_data)}")
-                print(f"Recieved Data: {received_data}")
-                print()
-                counter_corrupted += 1
-
-
-        server_socket.close()  # Schließen Sie den Server-Socket
+        server_socket.close()  # schließt den Sockel wenn der Thread geschlossen wird
 
 class DATALOGGER:
     """datalogger takes data from listener and saves it in corresponding files"""
     def __init__(self) -> None:
-        self.rawdata = "rawdata.csv"
-        self.formateddata = "data.csv"
-        self.header()
+        self.rawdata = []
 
-    def header(self):
-        f = open(self.formateddata, "a")
-        header = [
-            "timestamp save",
-            "id",
-            "timestampPacket",
-            [f'timestampOxy{i}' for i in range(6)],
-            [f'oxigen{i}' for i in range(6)],
-            [f'tempTube{i}' for i in range(6)],
-            [f'heaterPWM{i}' for i in range(6)],
-            "info",
-            "tempCpu",
-            "Voltage",
-            "Current",
-            "error",
-        ]
-        f.write("\n")
-        for elem in header:
-            if type(elem) is str:
-                f.writelines(elem)
-                f.write(";")
-            if type(elem) is list:
-                for i in elem:
-                    f.writelines(i)
-                    f.write(";")
-        f.write("\n")
-        f.close
-
-    def write_packet(self, packet):
+    def bintolist(binary_data,struct_format):
+        """convert binary to (struct)list"""
         try:
-            with open(self.formateddata, "a") as f:
-                f.write(packet[1])
-                f.write(";")
-                for elem in packet[0]:
-                    f.writelines(str(elem))
-                    f.write(";")
-                f.write("\n")
-                f.close
-                return 1
-        except FileNotFoundError:
-            print("File not found.")
-            return 0
-        except IOError:
-            print("Error reading the file.")
-            return 0
-    
-    def savepackets(self,TCP_server:TCP_SERVER):
-        """reads the packets from the buffer of the TCP_Server, reads them in data.csv and deletes the packets in TCP_Server if saving was successful"""
-        packets = TCP_server.packets
-        sucess = 1
-        for el in packets:
-            if not self.write_packet(el):
-                sucess = 0
-        if sucess:
-            TCP_server.packets = []
-        else:
-            print("packet convertion to data.csv went wrong")
-    
-    def write_rawdata(self,rawdata):
-        try:
-            with open(self.rawdata, "a") as f:
-                f.write(str(rawdata[1]))
-                f.write(";")
-                f.write(str(rawdata[0]))
-                f.write(";")
-                f.write("\n")
-                f.close
-                return 1
-        except FileNotFoundError:
-            print("File not found.")
-            return 0
-        except IOError:
-            print("Error reading the file.")
-            return 0
+            unpacked_data = struct.unpack(struct_format, binary_data) # Entpacken Sie die Daten in die Struktur
+            return(unpacked_data)
+        except:
+            print("\nstruct_format is wrong or data is corrupted")
+            print(f"Length of binary Data: {len(binary_data)}")
+            print(f"Binary Data: {binary_data}")
+            print()
         
-    def saveraw(self,TCP_server:TCP_SERVER):
+    def saveraw_csv(self):
         """reads the rawdata from the buffer of the TCP_Server, reads them in rawdata.csv and deletes the rawdata in TCP_Server if saving was successful"""
-        sucess = 1
-        for el in TCP_server.rawdata:
-            if not self.write_rawdata(el):
-                sucess = 0
-        if sucess:
-            TCP_server.rawdata = []
-        else:
-            print("rawdata convertion to rawdata.csv went wrong")
+        for el in self.rawdata:
+            try:
+                with open("./rawdata.csv", "a") as f:
+                    f.write(str(el[0]))
+                    f.write(";")
+                    f.write(str(el[1]))
+                    f.write(";")
+                    f.write("\n")
+                    f.close
+            except FileNotFoundError:
+                print("File not found.")
+                break
+            except IOError:
+                print("Error reading the file.")
+                break
+            else:
+                pass #delete the packages from
+        
 
-def writetomongo():
-    pass
-    
 
 if __name__ == "__main__":
-    
-    # 	a = TCP_SERVER()
-    # # 	# while True:
-    # 	time.sleep(2)
-    # 	print(a.packets[0])
-    # 	a.isRunning = 0
-
     test_packet = (
         3522,
         0,
@@ -271,17 +194,14 @@ if __name__ == "__main__":
         b"Hier steht die zu \xc3\xbcbertragende Info\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
     )
  
-    a = TCP_SERVER()
-    datalog = DATALOGGER()
-    mongodb = MongoDB("localhost:27017", "Probe", "Probecollect" )
-    mongodb.connect()
-    time.sleep(2)
+    server = TCP_SERVER()
+    datalog = server.datalog
+    # mongodb = MongoDB("localhost:27017", "Probe", "Probecollect" )
+    # mongodb.connect()
+    time.sleep(5)    
+    server.isRunning = 0
     
-    while True:
-        for packet in a.packets:
-            mongodb.write_mongodb({"test" : str(packet)})
-        a.packets = []
-        
-        datalog.savepackets(a)
-        datalog.saveraw(a)
-        time.sleep(2)
+    # for packet in datalog.rawdata:
+    #     mongodb.write_mongodb({"test" : str(packet)})
+    
+    datalog.saveraw_csv()
