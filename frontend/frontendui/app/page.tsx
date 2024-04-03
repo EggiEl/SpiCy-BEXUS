9dea9da0b8897202ffc0ca5e6cb31f5457e773c8
Elias Eggenberger <elias.eggenberger@hm.edu>
2024-04-03 19:13:44 +0200

    get_all_data() function for frontend implemented and tested

--- frontend/frontendui/app/page.tsx
+++ frontend/frontendui/app/page.tsx
@@ -2,12 +2,16 @@
 
 import MyResponsiveChart from '../components/plot_temp';
 import {get_latest_data} from '../functions/get_latest_data';
-
+import {get_all_data} from '../functions/get_all_data';
 
   export default function LandingPage() {
 
     const fetchData = async () => {
       const data = await get_latest_data("6601d39a57c1ff9b7787bd03", "Sensor1");
+      console.log(data);
+    }
+    const alldatat = async () => {
+      const data = await get_all_data() ;
       console.log(data);
     }
 
@@ -22,6 +26,8 @@ import {get_latest_data} from '../functions/get_latest_data';
           <button onClick={fetchData}>
         Daten abrufen
       </button>
+      <button onClick={alldatat}>
+Alle Daten      </button>
           <div style={{ height: '500px' }}>
           <MyResponsiveChart />
         </div>
