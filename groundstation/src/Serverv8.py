a6f982d38cf3c3cfc15eba0cd1d6580f5abde3c7
Fynn Gewiese <gewiese@hm.edu>
2024-08-20 23:13:19 +0200

    added function to add dummy data

--- groundstation/src/Serverv8.py
+++ groundstation/src/Serverv8.py
@@ -1,13 +1,13 @@
-from http import server
 import socket
 import struct
 import threading
 import time
+import random
 
-from zmq import NULL
 from MongoDB import MongoDB
 import netifaces
 from colored_terminal import *
+
 #convertion to exe: pyinstaller --onefile --distpath ./compiled .\src\Serverv8.py
 
 # troubleshooting tools:
@@ -41,10 +41,10 @@ class TCP_SERVER:
             "param3": 0.0,
             "param4": 0.0,
         }
-        self.thread = threading.Thread(target=self.StartServer)
+        self.thread = threading.Thread(target=self.start_server)
         self.thread.start()
 
-    def StartServer(self):
+    def start_server(self):
         """startet eine TCP Server an den sich ein client_socket verbinden kann"""
 
         def connect_server_socket(timeout=TIMEOUT_CLIENT):
@@ -55,16 +55,16 @@ class TCP_SERVER:
                 return server_socket
             
             except OSError as e:
-                if e.errno == 10048:print_cyan("Address already in use.\n", indent=0)
-                elif e.errno == 10049:print_cyan("Probably cable not connected.\n", indent=0)
-                elif e.errno == 10038:print_cyan("Ein Vorgang bezog sich auf ein Objekt, das kein Socket ist. Probably no Ethernet dongle\n", indent=0)
+                if e.errno == 10048:print_red("Address already in use.\n", indent=1)
+                elif e.errno == 10049:print_red("Probably cable not connected.\n", indent=1)
+                elif e.errno == 10038:print_red("Ein Vorgang bezog sich auf ein Objekt, das kein Socket ist. Probably no Ethernet dongle\n", indent=1)
                 else:print_red(f"Error:{e} ", indent=2)
 
                 time.sleep(DELAY_ERROR)
                 return None
                 
             except Exception as e:  # Handle other exceptions
-                print_red(f"Error Connect Server:{e}\n", indent=2)
+                print_red(f"Error Connect Server:{e}\n", indent=1)
                 time.sleep(DELAY_ERROR)
                 return None
 
@@ -73,26 +73,26 @@ class TCP_SERVER:
             try:
                 server_socket.listen(1)  # Wartet auf 1 eingehende Verbindung
                 client_socket, client_address = server_socket.accept()
+                client_socket.settimeout(timeout)
                 clear_console_line()
                 print_cyan(f"Verbindung hergestellt von:{client_address}\n", indent=1)
-                client_socket.settimeout(timeout)
                 return client_socket
             
             except socket.timeout:
                 clear_console_line()
-                print_cyan("waiting for a connection[" + counter * "."+"]")
+                print_cyan("waiting for a connection[" + counter * "."+"]", indent=1)
                 return None
             
             except OSError as e:
                 if e.errno == 10038: print_cyan("Error Connect Client: Probably no Ethernet dongle\n", indent=0)
-                else: print_red(f"Error Connect Client: {e} ", indent=2)
+                else: print_red(f"Error Connect Client: {e} ", indent=1)
 
                 time.sleep(DELAY_ERROR)
                 server_socket.close()
                 return None
 
             except Exception as e:  # Handle other exceptions
-                print_red(f"Error Connect Client: {e} ", indent=2)
+                print_red(f"Error Connect Client: {e} ", indent=1)
                 time.sleep(DELAY_ERROR)
                 server_socket.close()
                 return None
@@ -160,39 +160,34 @@ class TCP_SERVER:
                 param3 = float(self.command["param3"])
                 param4 = float(self.command["param4"])
                 uploadbuffer = comand.encode("utf-8")*3 + struct.pack("ffffffff", param1,param1,param2,param2,param3,param3,param4,param4)
-                print(f'sending command: {comand}|{param1}|{param2}|{param3}|{param4}|\nin bytes: {uploadbuffer}')
+                print_yellow(f'sending command: {comand}|{param1}|{param2}|{param3}|{param4}|\nin bytes: {uploadbuffer}\n',indent=1)
                 client_socket.sendall(uploadbuffer)
                 # deletes command from buffer of server
                 self.command = {"command": '0', "param1": 0.0, "param2": 0.0, "param3": 0.0, "param4": 0.0}
             except Exception as e:
-                print_red(f"Error:{e}\n", indent=2)
+                print_red(f"Error:{e}\n", indent=1)
                 client_socket.close()
 
         server_socket = None
         client_socket = None
-        counter_points = 0 #for thoes "waiting for client[...]" points
+        counter_client_points = 0 #for thoes "waiting for client[...]" points
 
         print_cyan(f'<<<<starting TCP server>>>>\n')
-        
-        print_white(f'ip_adress: {self.ipadress}\nport: {self.port}"\ntime: {time.asctime(time.localtime())}\n\n')
+        print_white(f'ip_adress: {self.ipadress}\nport: {self.port}\ntime: {time.asctime(time.localtime())}\n\n')
 
         while self.__isRunning:
             ##connecting Socket if none is already connected
             if not server_socket:
                 print_blue("Connnecting Socket...\n", indent=1)
                 server_socket = connect_server_socket()
-                if not server_socket:
-                    time.sleep(DELAY_ERROR)
+                if not server_socket: time.sleep(DELAY_ERROR)
             else:
                 ##connecting client if none is already connected
                 if not client_socket:
-                    client_socket = connect_client(
-                        server_socket, counter=counter_points
-                    )
-                    counter_points += 1
-                    if counter_points > 3: counter_points = 0
+                    client_socket = connect_client(server_socket, counter=counter_client_points)
+                    counter_client_points += 1
+                    if counter_client_points > 3: counter_client_points = 0
                 else:
-                    print(client_socket)
                     ##[up link] sending command to client if there is one in the self.command buffer 
                     if self.command["command"] != '0':
                         send_command(client_socket)
@@ -227,33 +222,29 @@ class DATALOGGER:
         self.__isRunning = 1
         self.filename = f'./rawdata[{"-".join(time.asctime().split()[1:4]).replace(":","-")}].bin' #"./rawdata.bin"
         self.rawdata = []
-        self.mongodb = MongoDB("localhost:27017").connect()
+        self.mongodb = MongoDB("localhost:27017")
+        self.mongodb.connect()
         self.thread = threading.Thread(target=self.SavingLoop)
         self.thread.start()
 
     def SavingLoop(self):
-        print_yellow(f'Saving bin to : {self.filename}\n')
+        print_yellow(f'Saving bin to : {self.filename}\n',indent=1)
         while self.__isRunning:
-            if len(self.rawdata)>1:
+            if len(self.rawdata)>=1:
                 self.save_raw_mongo()
                 if self.save_raw_bin():
-                    print(f"saved {len(self.rawdata)} packages")
+                    print_white(f"saved {len(self.rawdata)} packages",indent=3)
                     self.rawdata = []
             else:
                 time.sleep(DELAY_DATALOG_LOOP) #delay to conserve performance
 
     def save_raw_mongo(self):
-        sucess = 1
         for packet in self.rawdata:
             try:
-                # self.mongodb.write_mongodb({packet[0]: packet[1]}, "Probe", "Probecollect")
-                pass
+                self.mongodb.get_raw_data(packet)
             except Exception as e:
-                print(e)
+                print_red(e,indent=1)
                 break
-        else:
-            sucess = 0
-        return sucess
 
     def save_raw_bin(self):
         """reads the rawdata from the buffer of the TCP_Server, reads them in rawdata.csv and deletes the rawdata in TCP_Server if saving was successful"""
@@ -264,22 +255,22 @@ class DATALOGGER:
                     f.write(el)
                     f.close
             except FileNotFoundError:
-                print("File not found.")
+                print_red("File not found.",indent=1)
                 sucess = 0
                 break
             except IOError:
-                print(IOError)
-                print("Error reading the file.")
+                print_red(f'{IOError}',indent=1)
+                print_red("Error reading the file.",indent=1)
                 sucess = 0
                 break
             except Exception as e:
-                print(e)
+                print_red(e,indent=1)
                 sucess = 0
                 break
         return sucess
 
     def shutdown(self):
-        print_magenta("<shutting down Datalogger>\n")
+        print_magenta("<shutting down Datalogger>\n",indent=1)
         self.__isRunning = 0
         self.save_raw_bin()
         self.thread.join()
@@ -315,22 +306,21 @@ class INTERFACE:
             match command[1:2]:
                 case "?":
                     print_yellow("<Help>")
-                    print_yellow(
-                        """
-/r [optional: server ip adress] start server
+                    print_yellow("""
+/r [optional: server ip adress] [optional: server port] start server
 /s shutdown server
 /i get network info
 /t get threads info
 /q /quit quit this file
 /c for sending a command to client
-"""
-                    )
-
+/d [random data?] save dummy packet in MongoDB
+""")
+                
                 case "r":
                     if self.server is None:
                         param_ip_adress = command[3:].split()
-                        print(param_ip_adress)
-                        print(len(param_ip_adress))
+                        # print(param_ip_adress)
+                        # print(len(param_ip_adress))
 
                         if len(param_ip_adress)==0: 
                             self.server = TCP_SERVER(_ipadress=IP_SERVER)
@@ -339,11 +329,9 @@ class INTERFACE:
                         elif len(param_ip_adress)==2:
                             self.server = TCP_SERVER(_ipadress=str(param_ip_adress[0]),_port=int(param_ip_adress[1]))
                         else:
-                            print("sometgin broke as me")
-
+                            print("something broke as me")
                     else:
                         print_yellow("Server already running\n")
-
 
                 case "s":
                     self.server_shutdown()
@@ -376,10 +364,12 @@ class INTERFACE:
                     self.server_shutdown()
                     self.isRunning = 0
                     print_yellow("<all down>\n", indent=1)
+                
                 case "t":
                     print_yellow("Running threads: ")
                     for thread in threading.enumerate():
                         print(thread.name)
+                
                 case "c":
                     if self.server is None:
                         print_red("No Server connected\n")
@@ -399,6 +389,60 @@ class INTERFACE:
                         except Exception as e:
                             print(e)
 
+                case "d":
+                    if self.server is None:
+                        print_red("No Server connected\n")
+                    else:
+                        
+                        format_oxy = "14L "  # Format for one OxygenReadout struct
+                        format_packet = "2L 2f " + 6 * format_oxy + "12f 9f 6f 3f"
+
+                        length_packet = struct.calcsize(format_packet)
+
+                        dummy_values = []
+                        
+                        random_values = 0
+
+                        #reads out of command weather to save random or dummy data
+                        if len(command)>=4:
+                            param = int(command[3:4])
+                            print(param)
+                            random_values = param
+                            
+                        if random_values == 1:
+                            print_yellow("Saving random dummy data\n")
+                            # Go through the format string and generate appropriate random values
+                            for fmt in format_packet.split():
+                                if fmt.endswith('L'):
+                                    # If the format is 'L', generate random unsigned long values
+                                    dummy_values.extend(random.randint(0, 2**32-1) for _ in range(int(fmt[:-1])))
+                                elif fmt.endswith('f'):
+                                    # If the format is 'f', generate random float values
+                                    dummy_values.extend(random.uniform(0, 1) for _ in range(int(fmt[:-1])))
+                        else:
+                            print_yellow("Saving numbered dummy data(0,1,2,3,4,5,6)\n")
+                            # Go through the format string and generate appropriate random values
+                            counter = 0
+                            for fmt in format_packet.split():
+                                if fmt.endswith('L'):
+                                    # If the format is 'L', generate random unsigned long values
+                                    list_buffer = []
+                                    for _ in range(int(fmt[:-1])):
+                                        list_buffer.append(counter)
+                                        counter +=  1
+                                    dummy_values.extend(list_buffer)
+                                elif fmt.endswith('f'):
+                                    list_buffer = []
+                                    for _ in range(int(fmt[:-1])):
+                                        list_buffer.append(counter)
+                                        counter +=  1
+                                    # If the format is 'f', generate random float values
+                                    dummy_values.extend(list_buffer)                           
+
+                        # Pack the values according to the format string
+                        randombytes = struct.pack(format_packet, *dummy_values)
+                        self.server.datalog.rawdata.append(randombytes)
+                       
                 case _:
                     print_red(f"Unknown Command {command[1:]}\n")
 
